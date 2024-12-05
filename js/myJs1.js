bbadd9bbf0493f4f5ddcfdf934218c0db2b34310
pnh74 <pnhzz.sk@gmail.com>
2024-12-05 19:18:42 +0700

    Create myJs1.js

--- js/myJs1.js
+++ js/myJs1.js
@@ -1,16 +1,16 @@
 const textConfig = {
   text1: "He luu cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text3: "Cậu có yêu tớ không ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra là tớ hum làm bản đồ nữa:v",
   text5: "Cậu mơ à???",
   text6: "Yêu ơi là yêu <3",
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text9: "Vì HuyPhuong đẹp try vlllll",
+  text10: "Tớ biết mà ^^ I love you 3000",
   text11:
-    "Tối thứ 7 cậu qua nhà tớ chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Thứ 7 cậu qua nhà tớ chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
 };
 
